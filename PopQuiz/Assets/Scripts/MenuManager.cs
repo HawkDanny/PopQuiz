7183c0aa407e1f779b4d0a55d78a00867b22ad3c
JacobRoos <bopzor@gmail.com>
2017-03-29 20:40:35 -0400

    Created 2 scenes, one for the main menu and one for the game main menu hides the mouse cursor and buttons can be selected with a controller

---
+++ PopQuiz/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        Debug.Log("start game selected");
+        Application.LoadLevel("Scenes/Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit game selected");
+        Application.Quit();
+    }
+}
